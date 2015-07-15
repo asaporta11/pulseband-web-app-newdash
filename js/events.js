6a7d7f45323fe26ac412bc61983faed4d08f7db3
asaporta11 <arielle.saporta@gmail.com>
2015-07-15 12:02:44 -0500

    Revert "Revert "Input values update""
    This reverts commit a54a9c9c78cdc2e5f5592eab15cc84bf93b816f4.

--- js/events.js
+++ js/events.js
@@ -1,12 +1,7 @@
 var events = {
-  dragmove: function (d) {             
-    d3.select(this)
-      .attr("y", function(d) {
-        this.tip.html(d);
-        return d3.mouse(this)[1];
-      })
-      .attr("height", ( svgHeight - margins.bottom - d3.mouse(this)[1] ));
+  updateInputValues: function(rect, yValue){
+    var rectNumber = $(rect).closest('.rect').data('rect');
+    var roundedInputY = Math.round(yValue);
+    $('#rect'+ rectNumber).val(roundedInputY);
   }
 }
-
-  
